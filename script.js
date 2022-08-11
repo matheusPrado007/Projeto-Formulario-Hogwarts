c6b0e6495e4487c316e534dcd29cf27fff5adcf0
Matheus Prado <peu_prado01@hotmail.com>
2022-08-11 18:54:18 -0300

    19 e 20 concluidos, e 21 em construção

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ function agreement() {
   }
 }
 aceitar.addEventListener('click', agreement);
+
+const counter = document.getElementById('counter');
+const comentario = document.getElementById('textarea');
+
+function contador() {
+  counter.innerText = 500 - comentario.value.length;
+}
+comentario.addEventListener('keyup', contador);
+const btnEnviar = document.getElementById('submit-btn');
+const primeiroNome = document.getElementById('input-name');
+const segundoNome = document.getElementById('input-lastname');
+function enviar(event) {
+  event.preventDefault();
+  const nomeSobrenome = document.getElementById('nomeSobrenome');
+  nomeSobrenome.innerText = `Nome: ${primeiroNome.value} ${segundoNome.value}`;
+  const formulario = document.getElementById('evaluation-form');
+  formulario.innerHTML = '';
+}
+btnEnviar.addEventListener('click', enviar);
